9ffc4a2a14b3a479dae66fb1c7f2175adfc95569
peters139 <peters139@rowan.edu>
2025-04-19 15:04:18 -0400

    Update Ignitor_LED.c

--- Ignitor_LED.c
+++ Ignitor_LED.c
@@ -1,1 +1,25 @@
+#include <msp430.h>
+#include "Ignitor_LED.h"
 
+void ignitor_led_init(void)
+{
+    // Set P2.0 as output
+    P2DIR |= BIT0;
+    // Turn off LED initially
+    P2OUT &= ~BIT0;
+}
+
+void ignitor_led_on(void)
+{
+    P2OUT |= BIT0;  // Set P2.0 high
+}
+
+void ignitor_led_off(void)
+{
+    P2OUT &= ~BIT0; // Set P2.0 low
+}
+
+void ignitor_led_toggle(void)
+{
+    P2OUT ^= BIT0;  // Toggle P2.0
+}
