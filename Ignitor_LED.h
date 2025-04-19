28877d0bb8eb214455ac86c18f22e40d7484c0aa
Zachary767 <cornwe13@rowan.edu>
2025-04-19 15:39:15 -0400

    Update Ignitor_LED.h
    updated code

--- Ignitor_LED.h
+++ Ignitor_LED.h
@@ -1,6 +1,14 @@
-#define IGNITOR_LED_H_
+#ifndef IGNITOR_LED_H
+#define IGNITOR_LED_H
 
+#include <stdint.h>
+
+// Initialize P2.0 as output and turn the LED off
 void ignitor_led_init(void);
+
+// Control routines
 void ignitor_led_on(void);
 void ignitor_led_off(void);
 void ignitor_led_toggle(void);
+
+#endif // IGNITOR_LED_H
