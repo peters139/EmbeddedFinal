f6a1b3093c1a8e92ab9794de96284fc2d8d03c9c
Zachary767 <cornwe13@rowan.edu>
2025-04-19 15:56:53 -0400

    Update PilotValve.h

--- PilotValve.h
+++ PilotValve.h
@@ -1,1 +1,12 @@
+main_valve.h
+ifndef MAIN_VALVE_H
+define MAIN_VALVE_H
 
+include <stdint.h>
+/** P5.0 drives PWM output for servo */
+define MAIN_VALVE_PIN    BIT0
+void mainValve_init(void);
+
+void mainValve_set(uint8_t position);
+
+endif // MAIN_VALVE_H
