64fa64084c49bc4c9067e99846261cf3ebdead3a
Zachary767 <cornwe13@rowan.edu>
2025-04-19 16:00:26 -0400

    Update RGBLED.c

--- RGBLED.c
+++ RGBLED.c
@@ -1,24 +1,36 @@
-#include "msp430fr2355.h"
-#include <msp430.h>
+// rgb_led.c
+#include "rgb_led.h"
 
-void RGB_Init(){
-    P6DIR |= BIT0 | BIT1 | BIT2;                     // P6.0 and P6.1 and P6.2 output
-    P6SEL0 |= BIT0 | BIT1 |BIT2;                    // P6.0 and P6.1 and P6.2 options select
-    
-    TB3CCR0 = 1000-1;                         // PWM Period
-    TB3CCTL1 = OUTMOD_3;                      // CCR1 reset/set
-    TB3CCR1 = 750;                            // CCR1 PWM duty cycle
-    TB3CCTL2 = OUTMOD_3;                      // CCR2 reset/set
-    TB3CCR2 = 250;                            // CCR2 PWM duty cycle
-    TB3CCTL3 = OUTMOD_3;                      // CCR3 reset/set
-    TB3CCR3 = 100;                             // CCR3 PWM duty cycle
-    TB3CTL = TBSSEL__SMCLK | MC__UP | TBCLR;  // SMCLK, up mode, clear TBR
+void rgb_init(void) {
+    // Unlock GPIO power-on default high-impedance mode
+    PM5CTL0 &= ~LOCKLPM5;
+
+    // Configure P6.0-6.2 for Timer_B2 outputs
+    P6DIR  |= RGB_RED_PIN | RGB_GREEN_PIN | RGB_BLUE_PIN;
+    P6SEL0 |= RGB_RED_PIN | RGB_GREEN_PIN | RGB_BLUE_PIN;
+    P6SEL1 &= ~(RGB_RED_PIN | RGB_GREEN_PIN | RGB_BLUE_PIN);
+
+    // Timer_B2 setup: SMCLK, up mode, clear TAR
+    TB2CTL   = TBSSEL__SMCLK | MC__UP | TBCLR;
+    TB2CCR0  = RGB_PWM_PERIOD;
+    TB2CCTL1 = OUTMOD_7;  // Reset/Set on CCR1
+    TB2CCTL2 = OUTMOD_7;  // Reset/Set on CCR2
+    TB2CCTL3 = OUTMOD_7;  // Reset/Set on CCR3
+
+    // Initialize duty cycles to 0 (LEDs off)
+    TB2CCR1 = 0;
+    TB2CCR2 = 0;
+    TB2CCR3 = 0;
 }
 
+void rgb_set(uint8_t red, uint8_t green, uint8_t blue) {
+    TB2CCR1 = red;
+    TB2CCR2 = green;
+    TB2CCR3 = blue;
+}
 
-//change RGBLED takes in 0-255 and produces the correct duty cycle
-void changeRGBLED(char Red, char Green, cha Blue){
-    TB3CCR1 = Blue <<2; //Set Blue duty cycle
-    TB3CCR2 = Green <<2 //Set Green duty cycle
-    TB3CCR3 = Red <<2 // Set Red duty cycle
+void rgb_off(void) {
+    TB2CCR1 = 0;
+    TB2CCR2 = 0;
+    TB2CCR3 = 0;
 }
